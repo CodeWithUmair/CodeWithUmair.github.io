ffa0939a5992d64d66681e5d65ac3f2354b04eb4
CodeWithUmair <codewithumair867@gmail.com>
2024-01-22 00:57:37 +0500

    git profile readme updated

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,22 +2,22 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'codewithumair', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        sortBy: 'updated', // Sort projects by 'stars' or 'updated'
+        limit: 6, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
@@ -52,7 +52,7 @@ const CONFIG = {
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Umair Amir',
     description: '',
     imageURL: '',
   },
@@ -70,44 +70,48 @@ const CONFIG = {
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://www.umairamir.netlify.app',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'codewithumair867@gmail.com',
   },
   resume: {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'TypeScript',
+    'React.js',
+    'Next.js',
+    'Tailwind',
+    'Git',
+    'CSS',
+    'JavaScript',
+    'Node.js',
     'PHP',
     'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
     'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Ecommerce Inside 🔗',
+      position: 'CMS Developer',
+      from: 'March 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://www.ecommerceinside.co',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Textile Channel 🔗',
+      position: 'Web Developer & Assistant Shipment Planner',
+      from: 'September 2021',
+      to: 'March 2023',
+      companyLink: 'https://www.textilechannel.com/',
+    },
+    {
+      company: 'Freelancing ⚡',
+      position: 'Frontend Dev | Web3 | Shopify',
+      from: 'August 2022',
+      to: 'Present',
+      companyLink: '',
     },
   ],
   certifications: [
@@ -210,11 +214,13 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+  /**
+   footer: `Made with <a 
+       class="text-primary" href="https://github.com/arifszn/gitprofile"
+       target="_blank"
+       rel="noreferrer"
+     >GitProfile</a> and ❤️`,
+   */
 
   enablePWA: true,
 };
